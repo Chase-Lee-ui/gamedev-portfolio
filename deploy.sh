1039933d65e805abb94ddd8503c8091faa5b1e44
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2022-02-22 20:07:56 -1000

    change

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:Chase-Lee-ui/Chase-Lee-ui.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:Chase-Lee-ui/gamedev-portfolio.git main:gh-pages
+ git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 cd -
