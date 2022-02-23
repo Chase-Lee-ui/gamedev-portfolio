6bc5dcaa109c29cfe113f1ff73301518f7ba53b7
Chase Lee <chaselee65@gmail.com>
2022-02-22 18:27:05 -1000

    pog pushies

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -1,17 +1,24 @@
 import ProjectData from '@/data/ProjectData.ts'
 
 export default [
-    new ProjectData("project-1", "Optimistic Chubby", "img/projects/project-1-icon.png", 
+    new ProjectData("project-1", "Super Monkey Culture", "img/projects/project-1-icon.png",
     `
+
+    <div class="paragraph center">
+        <img class="big-image" src="img/coastarhome.png" alt="Coastar Screenshot" />
+    </div>
+    
     <div class="paragraph">
-     <strong>Optimistic Chubby</strong> is a thing of beauty that I am so proud of. It's available on Android because why not.
-     <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
+     <strong>Super Monkey Culture</strong> is a game project made with the Game Dev Club at UH, you play as a monkey that travels
+     through time gathering artifacts for a museum to study their cultural significance. This game was submitted for the 
+     <a href="https://itch.io/jam/cultural-heritage-game-jam" target="_blank">Cultural Heritage Game Jam</a>. This was a nice way to
+     give spotlight to a couple of culturals that are here present on Hawaii, while also getting my team to learn more about Unity.
     </div>
-    <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
-    </div>
-    <div class="paragraph center">
-        <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+    
+    <div class="paragraph">
+    The cultures that were given a spotlight in this game was Hawaiian, Mayan, and Filipino. The monkey had to set out and
+    retrieve an ancient Hawaiian pig, a Mayan tablet, and a Filipino amulet. These would be put on display in the museum
+    as a way to provide more insight into their culture.
     </div>
 
     <div class="paragraph">
@@ -28,8 +35,8 @@ export default [
         <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
         <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
     </div>
-    `, "#23bd69", false),
-    new ProjectData("project-2", "Singing Addict", "img/projects/project-2-icon.png", `
+    `, "#23bd69", false, true),
+    new ProjectData("project-2", "GGJ Game", "img/projects/project-2-icon.png", `
     <div class="paragraph">
         <strong>Optimistic Chubby Blog</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
         <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@adonyi-gabor-604571">Adonyi Gábor</a>.
@@ -61,8 +68,8 @@ export default [
     <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
     <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
     </div>
-    `, "#5a78af"),
-    new ProjectData("project-3", "Drawing Overload", "img/projects/project-3-icon.png", `
+    `, "#5a78af", true),
+    new ProjectData("project-3", "Drone Project", "img/projects/project-3-icon.png", `
     <div class="paragraph">
         <strong>Drawing Overload</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
         <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@miphotography">Miesha Maiden</a>.
@@ -94,8 +101,8 @@ export default [
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
     </div>
-    `, "#383838"),
-    new ProjectData("project-4", "Eugeneable", "img/projects/project-4-icon.png", `
+    `, "#383838", false, true),
+    new ProjectData("project-4", "Square-Bound", "img/projects/project-4-icon.png", `
     <div class="paragraph">
     <strong>Eugeneable</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
     <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@neo8iam">NEOSiAM 2020</a>.
@@ -122,7 +129,7 @@ export default [
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
     </div>
     `, "#e80fb7"),
-    new ProjectData("project-5", "Cloud Drew Land", "img/projects/project-5-icon.png", `
+    new ProjectData("project-5", "The Adventures of Sleepy Dan", "img/projects/project-5-icon.png", `
     <div class="paragraph">
         <strong>Cloud Drew Land</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
         <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@cottonbro">cottonbro</a>.
@@ -149,5 +156,5 @@ export default [
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
-    </div>`, "#e48246")
+    </div>`, "#e48246", false, true)
 ];
--- src/data/OtherProjectsData.ts
+++ src/data/OtherProjectsData.ts
@@ -1,108 +1,61 @@
 import ProjectData from '@/data/ProjectData.ts'
 
 export default [
-    new ProjectData("project-6", "Sultans Of Sing", "img/projects/project-6-icon.png", `
+    new ProjectData("project-6", "Coastar", "img/coastarhome.png", `
     
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" />
+    <div class="paragraph center">
+        <img class="big-image" src="img/coastarhome.png" alt="Coastar Screenshot" />
+    </div>
+    
     <div class="paragraph">
       <strong>Coastar</strong> was meant to be an application to be used by one of the organizations that were participating in the Hawaii Annual Coding Challenge. My team and I participated in the NIC Hawaii – Hawaii Marine Animal Response (HMAR) Sighting & Reporting App challenge, where we needed to create an app that would streamline the process for people who are around different parts of the island help identify animals that are in distress.
-      <br/>Image by <a href="https://www.pexels.com/fr-fr/@aldoalz" target="blank">Aldo Picaso</a>.
             </div>
-            <div class="paragraph center">
-              <a href="https://www.microsoft.com/en-us/p/some-app" target="_blank"><img src="img/projects/ms-store-logo.png" alt="MS Store button" /></a>
-          </div>
 
           <div class="paragraph">
-            Main features :
+            Related to this project:
             <ul>
-            <li>Some stuff</li>
-            <li>Some great stuff</li>
-            <li>More awesome stuff</li>
-            <li>And then some</li>
+            <li><a href="https://github.com/HACC2021/Big-Brain-Coders" target="_blank">Github Page</a></li>
+            <li><a href="https://devpost.com/software/big-brain-coders" target="_blank">Devpost Page</a></li>
+            <li><a href="https://chase-lee-ui.github.io/projects/coastar" target="_blank">Essay Page</a></li>
             </ul>
         </div>
-
         <div class="paragraph center">
-          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
-          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
-          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" /> 
-        </div>
+        <iframe class="youtube" src="https://www.youtube.com/embed/HTa7QpfoOkg" frameborder="0" allowfullscreen></iframe>
+    </div>
     `, "#c10606", false, true),
-    new ProjectData("project-7", "Drew Blood", "img/projects/project-7-icon.png", `
+    new ProjectData("project-7", "Covid-Sight", "img/covidsightheadliner.jpg", `
     <div class="paragraph">
-                <strong>Drew Blood</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-                <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@karolina-grabowska">Karolina Grabowska</a>.
+    <div class="paragraph center">
+        <img class="big-image" src="img/covidsight.png" alt="Coastar Screenshot" />
+    </div>
+                <strong>Covid-Sight</strong> was made to mirror the lumisight app that UH uses for daily covid checkups with all of its capabilities. This means that the Covid-Sight app needs to allow users to do a daily check-up, upload vaccine status, have a log in, and a clear vaccination status update. Additionally, in this app, there is a check up history and the ability to edit the history.
             </div>
-
-            <div class="paragraph">
-              Main features :
-              <ul>
-              <li>Some stuff</li>
-              <li>Some great stuff</li>
-              <li>More awesome stuff</li>
-              <li>And then some</li>
-              </ul>
-          </div>
-
-            <div class="paragraph">
-              <div class="notice">
-                You can download and try the app for free or purchase it directly from <a href="http://www.somewbsite.com/" target="_blank">Some website</a>
-              </div>
-            </div>
-
-            <div class="paragraph center">
-                <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" />
-                <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" />
-                <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" /> 
-            </div>`, "#1ca1e2"),
-    new ProjectData("project-8", "The Art of Walking", "img/projects/project-8-icon.png", `
-    <div class="paragraph">
-                <strong>The Art of Walking</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-            </div>
-
-            <div class="paragraph center">
-              <a href="https://play.google.com/store/apps/details?id=someapp" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
-          </div>
 
           <div class="paragraph">
-          Main features :
-          <ul>
-          <li>Some stuff</li>
-          <li>Some great stuff</li>
-          <li>More awesome stuff</li>
-          <li>And then some</li>
-          </ul>
-      </div>
-
-            <div class="paragraph">
-              <div class="notice">
-                Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
-              </div>
-            </div>
-
-            <div class="paragraph center">
-                <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="The Art of Walking Screenshot" />
-                <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="The Art of Walking Screenshot" />
-            </div>`),
-    new ProjectData("project-9", "Nikolable", "img/projects/project-9-icon.png", `
-     <div class="paragraph">
-                <strong>Nikolable</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-                <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@goumbik">Lukas</a>.
-            </div>
-
-            <div class="paragraph">
-        Main features :
-        <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
-        </ul>
+            Related to this project:
+            <ul>
+            <li><a href="https://covid-sight.github.io/" target="_blank">Github Page</a></li>
+            <li><a href="https://github.com/Covid-Sight" target="_blank">Github Organization Page</a></li>
+            <li><a href="https://chase-lee-ui.github.io/projects/Covid-Sight" target="_blank">Essay Page</a></li>
+            </ul>
+          </div>
+            </div>`, "#1ca1e2"),
+    new ProjectData("project-8", "Campus Clearout Center", "img/campus.jpg", `
+    <div class="paragraph center">
+        <img class="big-image" src="img/campusclearoutcenter.png" alt="Coastar Screenshot" />
     </div>
+    <div class="paragraph">
+                <strong>Campus Clearout Center</strong> is a project that allows UH Manoa community members to buy items that other members of the UH Manoa community are willing to part with for a small sum of cash. This program was made by the work of my teammates and I for an assigned project in out software engineering class.
+            </div>
 
-            <div class="paragraph">
-              <div class="notice">
-                Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
-              </div>
-            </div>`),
+          <div class="paragraph">
+          <div class="paragraph">
+            Related to this project:
+            <ul>
+            <li><a href="https://campus-clearout-center.github.io/" target="_blank">Github Page</a></li>
+            <li><a href="https://github.com/campus-clearout-center" target="_blank">Github Organization Page</a></li>
+            <li><a href="https://chase-lee-ui.github.io/projects/Campus-Clearout-Center" target="_blank">Essay Page</a></li>
+            </ul>
+          </ul>
+      </div>`),
 ];
